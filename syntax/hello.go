5ac212b1c8e07386d59a08bedef6f7f9b9803f65
Bhuppal Kumar <Bhuppal@hotmail.com>
2021-04-23 18:16:40 -0400

    Add Go Basic and Iteration

--- syntax/hello.go
+++ syntax/hello.go
@@ -6,11 +6,19 @@ import (
 	"strings"
 )
 
+/*
+Packages
+********
+fmt - provides functions for formatting text and for reading formatted text
+os - provides platform-independent operating system variables and functions
+strings - provides functions for manipulating strings
+*/
+
 func main() {
 
-	var argValues string //defining an argValues
+	var argValues string  //defining an argValues
 	if len(os.Args) > 1 { //checking the argument values for ex: go run hello.go hello bhuppal kumar
-		argValues = strings.Join(os.Args[1:], " ") 
+		argValues = strings.Join(os.Args[1:], " ")
 	}
 	fmt.Println(argValues)
 }
