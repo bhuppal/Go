7f07b15de8a014e8867dd973c5c55faf7e9e639b
bhuppal <bhuppal@hotmail.com>
2021-04-26 06:39:36 -0400

    Add pointersand struct

---
+++ GoTour/array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// An array's length is part of its type,
+// so arrays cannot be resized. T
+// This seems limiting, but don't worry;
+// Go provides a convenient way of working with arrays.
+
+func main() {
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "Bhuppal"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+}
